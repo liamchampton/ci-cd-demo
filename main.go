751d5d45df63bc3edab4633009373bcd0097181d
liamchampton <liam.hampton@hotmail.co.uk>
2020-11-26 16:41:44 +0000

    inital commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// Create the route handler listening on '/'
+	http.HandleFunc("/", Home)
+	fmt.Println("Starting server on port 8080")
+
+	// Start the sever
+	http.ListenAndServe(":8080", nil)
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	msg := "This is a test for liamstoolchain"
+	w.Write([]byte(fmt.Sprintf(msg)))
+}
